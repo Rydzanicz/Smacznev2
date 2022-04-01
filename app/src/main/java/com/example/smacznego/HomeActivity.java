27a86ae0715931e44e18d6e406c3238906c0388c
Rydzanicz <rydzanicz.mm@gmail.com>
2022-04-01 10:24:12 +0200

    LognAndRegistration

---
+++ app/src/main/java/com/example/smacznego/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.smacznego;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
